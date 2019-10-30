69bdad3005cdc603ca7b2be06c8e8984bb3385a9
Arman Yerkesh <a.yerkesh@gmail.com>
2019-10-30 14:00:57 +0200

    Initialize Posts.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Posts from './components/Posts'
+
+const header = () => {
+  return <header>I am a header</header>
+}
+
 function App() {
   return (
     <div className="App">
+      { header() }
       <h1>Dashboard application</h1>
+      <Posts/>
     </div>
   );
 }
