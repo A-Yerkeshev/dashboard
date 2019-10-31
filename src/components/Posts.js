f55143831dbf52a15b419fee7be0c31a65c79cfa
Arman Yerkesh <a.yerkesh@gmail.com>
2019-10-31 16:32:02 +0200

    Show loading spinner while new posts are loading

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useBottomScrollListener } from 'react-bottom-scroll-listener';
+import $ from 'jquery';
 
 import Post from './Post';
 
@@ -25,17 +26,21 @@ function Posts() {
 
   // Load more posts when bottom of the page is reached
   const loadMorePosts = useBottomScrollListener(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts?userId=` + (state.posts.length/10 + 1))
-      .then((response) => {
-        // Generate random data for posts
-        response.data.forEach((post) => {
-          generateRandomData(post);
+    if (state.posts.length < 100) {
+      $('.spinner').show();
+      axios.get(`https://jsonplaceholder.typicode.com/posts?userId=` + (state.posts.length/10 + 1))
+        .then((response) => {
+          // Generate random data for posts
+          response.data.forEach((post) => {
+            generateRandomData(post);
+          });
+          const posts = [...state.posts, ...response.data];
+          setState({
+            posts
+          });
         });
-        const posts = [...state.posts, ...response.data];
-        setState({
-          posts
-        });
-      });
+      }
+      $('.spinner').hide();
   });
 
   // Function to add new post
@@ -76,6 +81,9 @@ function Posts() {
   return (
     <div className="container posts-container">
       { displayPosts() }
+      <div className="fa-3x spinner">
+        <i className="fas fa-spinner fa-spin"></i>
+      </div>
     </div>
   );
 }
