ed7235e8f2d050cb48a64ccd8dd7c9645bd63c4e
Arman Yerkesh <a.yerkesh@gmail.com>
2019-11-13 10:54:49 +0200

    Add styles to registration form

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function Registration(props) {
   return (
-    <div>Registration component</div>
+    <div className="container registration">
+      <h2>Welcome to Dashboard App!</h2>
+      <form className="reg-form">
+        Username:<br/>
+        <input type="text" name="username"/><br/>
+        Password:<br/>
+        <input type="password" name="password"/><br/>
+        Repeat password:<br/>
+        <input type="password" name="rep-password"/><br/>
+      </form>
+    </div>
   )
 }
 
