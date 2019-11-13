6cebd05bb1ecef1fdc327dc7380f4d686e81db3a
Arman Yerkesh <a.yerkesh@gmail.com>
2019-11-13 11:29:27 +0200

    Toggle eye icon on click

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import $ from 'jquery';
 
 function Registration(props) {
+
+  const toggleEyeButton = () => {
+    const toggle = (event) => {
+      event.preventDefault();
+      $(event.target).toggle();
+      $(event.target).siblings().toggle();
+    }
+
+    return (
+      <button onClick={ toggle }>
+        <i className="fas fa-eye-slash"></i>
+        <i className="fas fa-eye"></i>
+      </button>
+    )
+  }
   return (
     <div className="container registration">
       <h2>Welcome to Dashboard App!</h2>
@@ -8,9 +24,9 @@ function Registration(props) {
         Username:<br/>
         <input type="text" name="username"/><br/>
         Password:<br/>
-        <input type="password" name="password"/><br/>
+        <input type="password" name="password"/>{ toggleEyeButton() }<br/>
         Repeat password:<br/>
-        <input type="password" name="rep-password"/><br/>
+        <input type="password" name="rep-password"/>{ toggleEyeButton() }<br/>
       </form>
     </div>
   )
