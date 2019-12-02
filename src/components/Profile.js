1d2086cecccf4370e3570b1c17803c183ee55d28
Arman Yerkesh <a.yerkesh@gmail.com>
2019-12-02 10:11:47 +0200

    style profile picture panel buttons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 
 function Profile(props) {
   const user = {
@@ -8,11 +9,26 @@ function Profile(props) {
     password: 'waaaa'
   }
 
+  const picturePanel = () => {
+    return (
+      <div id="pic-panel">
+        <form className="container">
+          <input id="pic-panel-input" type="file" name="pic" accept="image/*"/>
+          <input id="pic-panel-submit" type="submit" value="Change picture" />
+        </form>
+      </div>
+    )
+  }
+
+  const openPicturePanel = () => {
+    $('#pic-panel').show();
+  }
+
   return (
     <div className="container profile">
       <div className="prof-pic">
         <i className="fas fa-user"></i>
-        <button>Change picture</button>
+        <button onClick={ openPicturePanel }>Change picture</button>
       </div>
       <div className="prof-info">
         <h2>{ user.username }</h2>
@@ -20,6 +36,7 @@ function Profile(props) {
         <h4>Recent posts:</h4>
         <h4>Recent comments:</h4>
       </div>
+      { picturePanel() }
     </div>
   )
 }
