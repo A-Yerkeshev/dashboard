690cb8ec2b30ee8476dde022e674cb7b1022c7af
Arman Yerkesh <a.yerkesh@gmail.com>
2020-01-20 11:07:35 +0200

    Pass post data from Post component to PostEdit component

--- src/components/PostEdit.js
+++ src/components/PostEdit.js
@@ -2,18 +2,38 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 function PostEdit(props) {
+  const postId = props.post.id;
+  const userId = props.post.userId
+  const title = props.post.title.charAt(0).toUpperCase() + props.post.title.slice(1);
+  const body = props.post.body.charAt(0).toUpperCase() + props.post.body.slice(1);
+  const author = props.post.author;
+  const likes = props.post.likes;
+  const dislikes = props.post.dislikes;
+  const date = props.post.date;
+  const comments = props.post.comments;
   const user = props.user;
   const post = props.post;
 
-  // Protect route from access by wrong user
-  if (!user || user.id !== post.userId) {
+  // Protect route from unauthorized access
+  if (!user || user.id !== userId) {
     return (
-      <Redirect to={ `/post/${post.id}` } />
+      <Redirect to={ `/post/${postId}` } />
     )
   }
 
   return (
-    <h1>Edit post</h1>
+    <div className="post-edit container">
+      <form className="post-edit-form">
+        <label htmlFor="title">Title</label>
+        <input name="title" value={ title } />
+        <textarea value={ body } />
+      </form>
+      <span>{ date }</span><br/>
+      <span>{ author }</span><br/>
+      <i className="far fa-thumbs-up fa-2x"></i> { likes }
+      <i className="far fa-thumbs-down fa-2x"></i> { dislikes }
+      <i className="far fa-comment fa-2x"></i> { comments }
+    </div>
   )
 }
 
